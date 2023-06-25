ae00a4547f6f8395ef6f343c3a8023999150eb6d
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-06-25 14:22:20 +0700

    Update control.go

--- service/controller/control.go
+++ service/controller/control.go
@@ -11,8 +11,8 @@ import (
 	"github.com/xtls/xray-core/features/stats"
 	"github.com/xtls/xray-core/proxy"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/common/limiter"
+	"github.com/qtai2901/xrayr/api"
+	"github.com/qtai2901/xrayr/common/limiter"
 )
 
 func (c *Controller) removeInbound(tag string) error {
