86ea970b4f0e134dd5b59274e0c954fa4ef4a0f6
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-06-25 14:24:31 +0700

    Update outboundbuilder.go

--- service/controller/outboundbuilder.go
+++ service/controller/outboundbuilder.go
@@ -8,7 +8,7 @@ import (
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
 
-	"github.com/XrayR-project/XrayR/api"
+	"github.com/qtai2901/xrayr/api"
 )
 
 // OutboundBuilder build freedom outbound config for addOutbound
