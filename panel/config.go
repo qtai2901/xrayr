6cb5eeb9f466092cfb44f53b49dd919d92a3b30f
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-06-25 14:21:04 +0700

    Update config.go

--- panel/config.go
+++ panel/config.go
@@ -1,8 +1,8 @@
 package panel
 
 import (
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/service/controller"
+	"github.com/qtai2901/xrayr/api"
+	"github.com/qtai2901/xrayr/service/controller"
 )
 
 type Config struct {
