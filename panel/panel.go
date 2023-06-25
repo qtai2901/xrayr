1af3643451893cf6cb6996da2b26008f2643d3cc
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-06-25 14:27:48 +0700

    Update panel.go

--- panel/panel.go
+++ panel/panel.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"sync"
 
-	"github.com/XrayR-project/XrayR/api/newV2board"
-	"github.com/XrayR-project/XrayR/app/mydispatcher"
+	"github.com/qtai2901/xrayr/api/newV2board"
+	"github.com/qtai2901/xrayr/app/mydispatcher"
 
 	"github.com/imdario/mergo"
 	"github.com/r3labs/diff/v2"
@@ -17,15 +17,16 @@ import (
 	"github.com/xtls/xray-core/core"
 	"github.com/xtls/xray-core/infra/conf"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/pmpanel"
-	"github.com/XrayR-project/XrayR/api/proxypanel"
-	"github.com/XrayR-project/XrayR/api/sspanel"
-	"github.com/XrayR-project/XrayR/api/v2board"
-	"github.com/XrayR-project/XrayR/api/v2raysocks"
-	_ "github.com/XrayR-project/XrayR/main/distro/all"
-	"github.com/XrayR-project/XrayR/service"
-	"github.com/XrayR-project/XrayR/service/controller"
+	"github.com/qtai2901/xrayr/api"
+	"github.com/qtai2901/xrayr/api/pmpanel"
+	"github.com/qtai2901/xrayr/api/proxypanel"
+	"github.com/qtai2901/xrayr/api/sspanel"
+	"github.com/qtai2901/xrayr/api/v2board"
+	"github.com/qtai2901/xrayr/api/xflash"
+	"github.com/qtai2901/xrayr/api/v2raysocks"
+	_ "github.com/qtai2901/xrayr/main/distro/all"
+	"github.com/qtai2901/xrayr/service"
+	"github.com/qtai2901/xrayr/service/controller"
 )
 
 // Panel Structure
@@ -182,6 +183,8 @@ func (p *Panel) Start() {
 			apiClient = v2board.New(nodeConfig.ApiConfig)
 		case "NewV2board":
 			apiClient = newV2board.New(nodeConfig.ApiConfig)
+		case "Xflash":
+			apiClient = xflash.New(nodeConfig.ApiConfig)
 		case "PMpanel":
 			apiClient = pmpanel.New(nodeConfig.ApiConfig)
 		case "Proxypanel":
