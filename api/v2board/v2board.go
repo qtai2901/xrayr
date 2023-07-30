e1a54ec81f5e59b0a9113ca2b0ca824872d730a8
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-07-30 21:01:02 +0700

    Update v2board.go

--- api/v2board/v2board.go
+++ api/v2board/v2board.go
@@ -238,30 +238,32 @@ func (c *APIClient) GetUserList() (UserList *[]api.UserInfo, err error) {
 
 // ReportNodeOnlineUsers reports online user ip
 func (c *APIClient) ReportNodeOnlineUsers(onlineUserList *[]api.OnlineUser) error {
-	c.access.Lock()
-	defer c.access.Unlock()
-
-	reportOnline := make(map[int]int)
+	var path string
+	switch c.NodeType {
+	case "V2ray":
+		path = "/api/v1/server/Deepbwork/onlinde"
+	case "Vless":
+		path = "/api/v1/server/Deepbwork/onlinde"
+	case "Trojan":
+		path = "/api/v1/server/TrojanTidalab/online"
+	case "Shadowsocks":
+		path = "/api/v1/server/ShadowsocksTidalab/online"
+	default:
+		return nil, fmt.Errorf("unsupported Node type: %s", c.NodeType)
+	}
 	data := make([]OnlineUser, len(*onlineUserList))
 	for i, user := range *onlineUserList {
 		data[i] = OnlineUser{UID: user.UID, IP: user.IP}
-		if _, ok := reportOnline[user.UID]; ok {
-			reportOnline[user.UID]++
-		} else {
-			reportOnline[user.UID] = 1
-		}
 	}
-	c.LastReportOnline = reportOnline // Update LastReportOnline
+	postData := &PostData{Type: nodeType, NodeId: c.NodeID, Onlines: data}
+	
 
-	postData := &PostData{Data: data}
-	path := fmt.Sprintf("/mod_mu/users/aliveip")
 	res, err := c.client.R().
-		SetQueryParam("node_id", strconv.Itoa(c.NodeID)).
+		SetHeader("Content-Type", "application/json").
 		SetBody(postData).
 		SetResult(&Response{}).
 		ForceContentType("application/json").
 		Post(path)
-
 	_, err = c.parseResponse(res, path, err)
 	if err != nil {
 		return err
@@ -274,6 +276,8 @@ func (c *APIClient) ReportUserTraffic(userTraffic *[]api.UserTraffic) error {
 	var path string
 	switch c.NodeType {
 	case "V2ray":
+		path = "/api/v1/server/Deepbwork/submit"
+	case "Vless":
 		path = "/api/v1/server/Deepbwork/submit"
 	case "Trojan":
 		path = "/api/v1/server/TrojanTidalab/submit"
