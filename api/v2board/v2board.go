9c19572d78880c49f46f4f2b63e0d2b62968e948
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2023-10-23 19:10:03 +0700

    Update v2board.go

--- api/v2board/v2board.go
+++ api/v2board/v2board.go
@@ -216,13 +216,10 @@ func (c *APIClient) GetUserList() (UserList *[]api.UserInfo, err error) {
 	for i := 0; i < numOfUsers; i++ {
 		user := api.UserInfo{}
 		user.UID = response.Get("data").GetIndex(i).Get("id").MustInt()
-		// speed =  response.Get("data").GetIndex(i).Get("speed_limit").MustInt()
-		// if c.SpeedLimit > 0 {
-		// 	user.SpeedLimit = uint64(c.SpeedLimit * 1000000 / 8)
-		// } else {
-			
-		// }
-		user.SpeedLimit = uint64(response.Get("data").GetIndex(i).Get("speed_limit").MustInt())
+		
+		
+	user.SpeedLimit = uint64(60 * 1000000 / 8)
+		
 			
 		user.DeviceLimit = c.DeviceLimit
 		switch c.NodeType {
